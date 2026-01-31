38ade0de208301c1d3c06251bac992d28d05cc68
Aaron Baggot <aaronbaggot@example.com>
2026-01-31 18:25:10 +0000

    initialise accommodation map layer icon for wen map - trails_map.js

--- trails_api/static/trails_api/js/trails_map.js
+++ trails_api/static/trails_api/js/trails_map.js
@@ -250,6 +250,19 @@ function initializeMap() {
   window.trailMarkers = L.layerGroup().addTo(window.trailsMap);
   console.log("✅ Trail marker layer added to map");
 
+  window.accommodationLayer = L.layerGroup().addTo(window.trailsMap);
+  console.log("✅ Accommodation layer added to map");
+
+  // Custom icon for hotels
+  window.hotelIcon = L.icon({
+    iconUrl: "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-orange.png",
+    shadowUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41],
+  });
+
   //  Custom green icon
   const defaultGreenIcon = L.icon({
     iconUrl:
