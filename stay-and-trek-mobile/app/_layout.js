9e1e7d97232af232a695a8b2333fa506d5108b26
Aaron Baggot <aaronbaggot@example.com>
2026-02-20 18:17:31 +0000

    Updated landing page to login

--- stay-and-trek-mobile/app/_layout.js
+++ stay-and-trek-mobile/app/_layout.js
@@ -1,9 +1,9 @@
-import { Stack } from 'expo-router';
-import { useEffect, useState } from 'react';
+import { Stack, useRouter, useSegments } from 'expo-router';
+import { useEffect, useState, createContext, useContext } from 'react';
 import { StatusBar, ActivityIndicator, View } from 'react-native';
 import * as SecureStore from 'expo-secure-store';
 import { AccessibilityProvider } from '../context/AccessibilityContext';
-import { createContext, useContext } from 'react';
+
 
 const AuthContext = createContext({
   userToken: null,
@@ -15,46 +15,64 @@ export const useAuth = () => useContext(AuthContext);
 export default function RootLayout() {
   const [userToken, setUserToken] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
+  const segments = useSegments();
+  const router = useRouter();
+  
 
   useEffect(() => {
+
     const loadToken = async () => {
       try {
         const token = await SecureStore.getItemAsync('userToken');
-        console.log('🔑 Loaded token from SecureStore:', token ? 'EXISTS' : 'NONE');
-        console.log('🔐 Current userToken state:', token);
+
         setUserToken(token);
+
       } catch (e) {
         console.log('❌ Error loading token:', e);
-      }
+      } finally {
       setIsLoading(false);
+      }
     };
     loadToken();
   }, []);
+
+useEffect(() => {
+  
+  if (isLoading) return;
+  
+  const inAuthGroup = segments[0] === '(auth)';
+
+  const isPublicRoute =
+    segments.includes('map') ||
+    segments.includes('trails') ||
+    segments.includes('weather');
+
+  if (!userToken && !inAuthGroup && !isPublicRoute) {
+    console.log('No token found, redirecting to login');
+    router.replace('/(auth)/login');
+  }
+
+}, [segments, userToken, isLoading]);
 
   if (isLoading) {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         <ActivityIndicator size="large" color="#63755fff" />
       </View>
+
     );
   }
+
 
   return (
     <AccessibilityProvider>
       <StatusBar backgroundColor="#63755fff" barStyle="light-content" />
       <AuthContext.Provider value={{ userToken, setUserToken }}>
         <Stack screenOptions={{ headerShown: false }}>
-          {!userToken ? (
-            <Stack.Screen name="(auth)" />
-          ) : (
             <Stack.Screen name="(app)" />
-          )}
+            <Stack.Screen name="(auth)" />   
         </Stack>
       </AuthContext.Provider>
     </AccessibilityProvider>
   );
 }
-
-
-
-
